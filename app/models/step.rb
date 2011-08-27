08cec44e92a2e77d05aee7ea32dfb8c9e06a9c77
Chris Yuska <chrisyuska@gmail.com>
2011-08-27 01:48:48 -0400

    Removed capitalization for now since it lowercases too much

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,12 +4,4 @@ class Step < ActiveRecord::Base
   validates_presence_of :arrival, :departure, :location
   geocoded_by :location, :latitude => :lat, :longitude => :lng
   after_validation :geocode
-  
-  before_save :make_pretty
-
-  private
-
-  def make_pretty
-    self.location = self.location.capitalize
-  end
 end
